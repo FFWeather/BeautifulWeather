a4ee3292288cac7ec5e8a1d57797c469fb3863f6
austin1000 <austin1000@foxmail.com>
2018-05-27 23:22:47 +0800

    添加天气界面（未完成）

---
+++ app/src/main/java/com/beautifulweather/android/gson/Forecast.java
@@ -0,0 +1,23 @@
+package com.beautifulweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Forecast {
+    public String date;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More{
+        @SerializedName("txt_d")
+        public String info;
+    }
+
+    @SerializedName("tmp")
+    public Temperature temperature;
+
+    public class Temperature{
+        public String max;
+        public String min;
+    }
+}
